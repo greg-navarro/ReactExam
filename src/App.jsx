4e4ba8063daba87e960add4d502e95339c49aa35
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-13 15:45:42 -0700

    make a storeInput component and add it to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { hot } from 'react-hot-loader';
 import React from 'react';
 import './App.css';
 
+import storeInput from './components/storeInput';
+
 const message = 'Welcome to ReactExam1';
 const App = () => (
   <div className="App">
     <h1>{message}</h1>
+    <br />
+    <storeInput />
   </div>
 );
 
---
+++ src/components/storeInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Input } from 'antd';
+
+const storeInput = () => (
+  <div>
+    <p>asdf</p>
+    <Input placeholder="Basic usage" />
+  </div>
+);
+
+export default storeInput;
