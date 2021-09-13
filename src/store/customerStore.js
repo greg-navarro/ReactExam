fdbd273586c34b5bbf441072255a66b93f599218
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-13 15:51:22 -0700

    Revert "attempt to fix customerstore"
    This reverts commit bf4bde2b73acd23a0a5ab12f26445198810ac19b.

--- src/store/customerStore.js
+++ src/store/customerStore.js
@@ -1,11 +1,19 @@
-class CustomerStore {
-  constructor() {
-    makeAutoObservable(this);
 
-    this.customerCode = customerCode ;
-    this.customerName = customerName ;
-    this.customerPhone = customerPhone;
-    this.customerCity = customerCity ;
-    this.customerState = customerState;
-  }
+class CustomerStore {
+    customerCode = ''
+    customerName = ''
+    customerPhone = ''
+    customerCity = ''
+    customerState = ''
+
+    constructor() {
+        makeAutoObservable(this)
+
+        this.customerCode = customerCode ;
+        this.customerName = customerName ;
+        this.customerPhone = customerPhone;
+        this.customerCity = customerCity ;
+        this.customerState = customerState;
+    }
+
 }
